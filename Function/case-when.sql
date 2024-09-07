4fdd742ca62fe5368627357645d1b96bd8293b06
Natasha Zhou <35412065+cottoncloud636@users.noreply.github.com>
2024-09-06 19:12:19 -0700

    Create case-when.sql

---
+++ Function/case-when.sql
@@ -0,0 +1,10 @@
+-- case when, equivalent to multiple if/else statements
+-- basic syntax: 
+/* CASE
+WHEN condition1 THEN result1
+WHEN condition2 THEN result2
+WHEN conditionN THEN resultN
+ELSE result
+END
+*/
+
